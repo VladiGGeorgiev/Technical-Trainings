e2900c9880ecede9ed41318a7dc32bb68593b92a
VGGeorgiev <vggeorgiev@mail.bg>
2015-06-09 10:43:19 +0300

    Added Social Network WebAPI Services

---
+++ AngularJS/Social-Network-REST-Services/SocialNetwork.Services/Models/Posts/PostViewModel.cs
@@ -0,0 +1,51 @@
+﻿namespace SocialNetwork.Services.Models.Posts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using SocialNetwork.Services.Models.Users;
+    using SocialNetwork.Services.Models.Comments;
+    using SocialNetwork.Models;
+
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+
+        public UserViewModelPreview Author { get; set; }
+
+        public UserViewModelPreview WallOwner { get; set; }
+
+        public string PostContent { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int LikesCount { get; set; }
+
+        public bool Liked { get; set; }
+
+        public int TotalCommentsCount { get; set; }
+
+        public IEnumerable<CommentViewModel> Comments { get; set; }
+
+        public static PostViewModel Create(Post p, ApplicationUser currentUser)
+        {
+            return new PostViewModel()
+            {
+                Id = p.Id,
+                Author = UserViewModelPreview.Create(p.Author, currentUser),
+                WallOwner = UserViewModelPreview.Create(p.WallOwner, currentUser),
+                PostContent = p.Content,
+                Date = p.Date,
+                LikesCount = p.Likes.Count,
+                Liked = p.Likes
+                    .Any(l => l.UserId == currentUser.Id),
+                TotalCommentsCount = p.Comments.Count,
+                Comments = p.Comments
+                    .Reverse()
+                    .Take(3)
+                    .Select(c => CommentViewModel.Create(c, currentUser))
+            };
+        }
+    }
+}
---
+++ AngularJS/Social-Network-REST-Services/SocialNetwork.Services/Models/Users/UserViewModelMinified.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.Ajax.Utilities;
+
+namespace SocialNetwork.Services.Models.Users
+{
+    using SocialNetwork.Models;
+using System;
+
+    public class UserViewModelMinified
+    {
+        //public static Expression<Func<ApplicationUser, UserViewModelMinified>> ViewModel
+        //{
+        //    get
+        //    {
+        //        return user => new UserViewModelMinified()
+        //        {
+        //            Id = user.Id,
+        //            Name = user.Name,
+        //            Username = user.UserName,
+        //            Gender = user.Gender,
+        //            ProfileImageData = user.ProfileImageDataMinified
+        //        };
+        //    }
+        //}
+
+        public string Id { get; set; }
+
+        public string ProfileImageData { get; set; }
+
+        public string Username { get; set; }
+
+        public string Name { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public static UserViewModelMinified Create(ApplicationUser user)
+        {
+            return new UserViewModelMinified()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Username = user.UserName,
+                Gender = user.Gender,
+                ProfileImageData = user.ProfileImageDataMinified
+            };
+        }
+    }
+}
